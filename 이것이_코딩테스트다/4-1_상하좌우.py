0d827d830e829563315ece8867c79e95f7cd294b
yejikim00 <kimyeji2902@gmail.com>
2022-01-31 02:28:58 +0900

    구현 -  4-1_상하좌우

--- 이것이_코딩테스트다/4-1_상하좌우.py
+++ 이것이_코딩테스트다/4-1_상하좌우.py
@@ -1,2 +1,28 @@
 N = int(input())
-print(N)
+order = input().split()
+row = 1
+col = 1
+
+
+for o in order:
+    if o == 'L':
+        col = col - 1
+        if col <= 0:
+            col = 1
+
+    elif o == 'R':
+        col = col + 1
+        if col > N:
+            col = N
+
+    elif o == 'U':
+        row = row - 1
+        if row <= 0:
+            row = 1
+
+    elif o == 'D':
+        row = row + 1
+        if row > N:
+            row = N
+
+print('{} {}'.format(row, col))
