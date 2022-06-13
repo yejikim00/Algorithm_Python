8c1fc847acce04b522dbef467e176a0dddb8fda8
yejikim00 <kimyeji2902@gmail.com>
2022-06-13 22:43:47 +0900

    Level 1 로또의 최고 순위와 최저 순위

--- programmers/Level1.py
+++ programmers/Level1.py
@@ -36,5 +36,60 @@ def solution1(id_list, report, k):
     return answer
 
 
-print(solution1(["muzi", "frodo", "apeach", "neo"], ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"], 2))
-print(solution1(["con", "ryan"], ["ryan con", "ryan con", "ryan con", "ryan con"], 3))
+# print(solution1(["muzi", "frodo", "apeach", "neo"], ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"], 2))
+# print(solution1(["con", "ryan"], ["ryan con", "ryan con", "ryan con", "ryan con"], 3))
+
+
+# Level 1 - 로또의 최고 순위와 최저 순위: 내 풀이
+def result_count(count):
+    if count == 6:
+        result = 1
+    elif count == 5:
+        result = 2
+    elif count == 4:
+        result = 3
+    elif count == 3:
+        result = 4
+    elif count == 2:
+        result = 5
+    else:
+        result = 6
+
+    return result
+
+
+def solution2(lottos, win_nums):
+    answer = []
+    count = 0
+    num_zero = lottos.count(0)
+    for l in lottos:
+        if l in win_nums:
+            count += 1
+
+    answer.append(result_count(count))
+    count += num_zero
+    answer.append(result_count(count))
+    answer.sort()
+    return answer
+
+
+# print(solution2([44, 1, 0, 0, 31, 25], [31, 10, 45, 1, 6, 19]))
+# print(solution2([0, 0, 0, 0, 0, 0], [38, 19, 20, 40, 15, 25]))
+# print(solution2([45, 4, 35, 20, 3, 9], [20, 9, 3, 45, 4, 35]))
+
+
+# Level 1 - 로또의 최고 순위와 최저 순위: 찾은 풀이
+def solution3(lottos, win_nums):
+    rank = [6, 6, 5, 4, 3, 2, 1]
+
+    cnt_0 = lottos.count(0)
+    ans = 0
+    for x in win_nums:
+        if x in lottos:
+            ans += 1
+
+    return [rank[cnt_0 + ans], rank[ans]]
+
+print(solution3([44, 1, 0, 0, 31, 25], [31, 10, 45, 1, 6, 19]))
+print(solution3([0, 0, 0, 0, 0, 0], [38, 19, 20, 40, 15, 25]))
+print(solution3([45, 4, 35, 20, 3, 9], [20, 9, 3, 45, 4, 35]))
